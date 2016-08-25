2d44e5fa1ea4bbe6f7aa3935d5d4a2db486a2a0f
at-ngatt <hangngaquay123@gmail.com>
2016-08-25 17:09:08 +0700

    fix migration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,17 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RolesTableSeeder::class);
-        $this->call(GroupTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(BooksTableSeeder::class);
-        $this->call(CategoriesLessonsTableSeeder::class);
-        $this->call(EntertainmentsTableSeeder::class);
-        $this->call(LessonsTableSeeder::class);
-        $this->call(LessonsDetailsTableSeeder::class);
-        $this->call(TestsTableSeeder::class);
-        $this->call(TestsLessonsTableSeeder::class);
-        $this->call(UsersTestTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
     }
 }
