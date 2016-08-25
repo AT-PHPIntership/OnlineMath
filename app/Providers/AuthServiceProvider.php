f3c17f76835b00386b8aae80b353a2724e49e21c
at-ngatt <hangngaquay123@gmail.com>
2016-08-25 01:53:25 +0000

    fix phpcs, phpmd

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Providers;
+
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -22,6 +24,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(GateContract $gate)
     {
         $this->registerPolicies($gate);
-        
     }
 }
